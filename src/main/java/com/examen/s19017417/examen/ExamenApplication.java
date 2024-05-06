4e73e9da080a0ac3b9244e1b3c5b60965fd572ac
EdZesty <eder.bergut@gmail.com>
2024-05-06 13:05:55 -0600

    implementando menu

---
+++ src/main/java/com/examen/s19017417/examen/ExamenApplication.java
@@ -0,0 +1,41 @@
+package com.examen.s19017417.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+		Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        do {
+            System.out.println("Menú de Tablas de Multiplicar:");
+            System.out.println("1. Mostrar una tabla");
+            System.out.println("2. Mostrar tablas del 2 al 5");
+            System.out.println("3. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el número de la tabla que desea ver: ");
+                    int numeroTabla = scanner.nextInt();
+                    mostrarTabla(numeroTabla);
+                    break;
+                case 2:
+                    mostrarTablasDelDosAlCinco();
+                    break;
+                case 3:
+                    System.out.println("¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
+            }
+
+        } while (opcion != 3);
+	}
+
+}
